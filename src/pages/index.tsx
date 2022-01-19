c36bb1c32771f50d065de8a82ad4bec3d8d43839
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-01-19 13:49:34 -0300

    fix layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({product} : HomeProps) {
     <main className={styles.contentContainer}>
       <section className={styles.hero}> 
         <span>👏🏽 Hey, welcome</span>
-        <h1>News about the <span>React</span>world.</h1>
+        <h1>News about the <span>React </span>world.</h1>
         <p>
           Get acess to all the publications<br/>
           <span>for {product.amount} month</span>
