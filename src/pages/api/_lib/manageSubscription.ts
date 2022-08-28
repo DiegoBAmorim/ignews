37a2768b02e02b101f92a4aa1e850b690c12ea9c
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-08-28 01:13:14 -0300

    finish webhooks

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,7 +4,8 @@ import { stripe } from '../../../services/stripe';
 
 export async function saveSubscription(
     subscriptionId: string,
-    customerId: string
+    customerId: string,
+    createAction = false
 ){
   //Buscar o usuario no banco do fauna com o ID{CustomerID}
   // Salvar os dados da subscription no FaunaDB
@@ -30,11 +31,29 @@ export async function saveSubscription(
 
    }
 
-   await fauna.query(
-    q.Create(
+   if(createAction){
+    await fauna.query(
+      q.Create(
         q.Collection('subscriptions'),
         {data: subscriptionData}
+     )
     )
-   )
+   }else{
+    await fauna.query(
+        q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+               q.Index('subscription_by_id'),
+               subscriptionId,
+            )
+          )
+        ),
+        { data: subscriptionData }
+        )
+    )
+   }
+  
 
 }
